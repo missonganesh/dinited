3e5fdc4c43e4c3f3dd9408355ab82fe309f50104
Ganesh Kadam <kadamgan@gmail.com>
2024-02-23 20:32:21 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,40 @@
+if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
+  throw new Error(`
+    Please provide a valid WordPress instance URL.
+    Add to your environment variables WORDPRESS_API_URL.
+  `);
+}
+
+const { protocol, hostname, port, pathname } = new URL(
+  process.env.WORDPRESS_API_URL,
+);
+
+/** @type {import('next').NextConfig} */
+/* module.exports = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '',
+        pathname: 'uploads/**',
+      },
+    ],
+	domains: [
+            "localhost"            
+    ],
+  },
+}; */
+module.exports = {
+    reactStrictMode: true,
+    images: {
+        domains: [
+            "localhost",
+            process.env.WORDPRESS_API_URL.match(/(http(?:s)?:\/\/)(.*)/)[2], // Valid WP Image domain.
+            "2.gravatar.com",
+            "0.gravatar.com",
+            "secure.gravatar.com",
+        ],
+    },
+};
